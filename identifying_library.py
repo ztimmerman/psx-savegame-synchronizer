31ee147177e2e897554bbd16ac449e468dd23408
= <=>
2022-06-20 08:38:56 -0400

    Added lengths of PSP and PS1 memory cards.

--- identifying_library.py
+++ identifying_library.py
@@ -5,3 +5,5 @@
 def f_identifyFile(file_data):
     MC_MAGIC = b'MC' #magic bit that starts a PS1 memory card
     SC_MAGIC = b'SC'
+    PS1_STANDARD_MC_LEN = 128*1024 #Standard PS1 memory card is 128 KB long
+    PSP_MC_LEN = 129*1024   #Length of the PSP memory card files
